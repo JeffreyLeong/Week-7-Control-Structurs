7f6f2eb01430ff7afb48fe8680a7fe2c3cde030c
dollarortwo <jleong001@gmail.com>
2022-03-15 20:18:46 -0700

    changed variables

--- ControlStructures.py
+++ ControlStructures.py
@@ -1,11 +1,11 @@
 vaccinationNames = ['pfizer-alpha', 'moderna-alpha', 'johnson-alpha', 'pfizer-kappa', 'pfizer-omega', 'moderna-delta', 'moderna-gamma', 'moderna-zeta'] 
 
-question1 = input("Please enter your name: ")
-question2 = input("Please enter your age: ")
-question3 = input("Do you have a medical condition?: ")
-question4 = input("Are you a teacher?: ")
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+medicalCondition = input("Do you have a medical condition?: ")
+teacher = input("Are you a teacher?: ")
 
-if ((question2 >= str(65)) and (question3 == 'yes') and (question4 == 'yes')):
+if ((age >= str(65)) and (medicalCondition == 'yes') and (teacher == 'yes')):
     print("You are eligible for a booster vaccination.")
 else:
     print("You are not eligble for a booster vaccination.")
